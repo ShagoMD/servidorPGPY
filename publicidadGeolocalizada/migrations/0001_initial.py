8e1e9918a86daafbc62606f838d62f0a2a929a0e
mehanika <mehanika91@gmail.com>
2013-01-29 23:03:17 -0600

    Creando modelos de la base de datos

--- publicidadGeolocalizada/migrations/0001_initial.py
+++ publicidadGeolocalizada/migrations/0001_initial.py
@@ -8,60 +8,13 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding model 'PuntoDeInteres'
-        db.create_table('publicidadGeolocalizada_puntodeinteres', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('nombre', self.gf('django.db.models.fields.CharField')(max_length=70)),
-            ('descripcion', self.gf('django.db.models.fields.CharField')(max_length=250)),
-            ('telefono', self.gf('django.db.models.fields.CharField')(max_length=10)),
-            ('paginaWeb', self.gf('django.db.models.fields.CharField')(max_length=80)),
-            ('correoElectronico', self.gf('django.db.models.fields.CharField')(max_length=80)),
-            ('rutaImagen', self.gf('django.db.models.fields.CharField')(max_length=150)),
-            ('posicion', self.gf('django.contrib.gis.db.models.fields.PointField')()),
-        ))
-        db.send_create_signal('publicidadGeolocalizada', ['PuntoDeInteres'])
-
-        # Adding model 'Anuncio'
-        db.create_table('publicidadGeolocalizada_anuncio', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('titulo', self.gf('django.db.models.fields.CharField')(max_length=70)),
-            ('descripcion', self.gf('django.db.models.fields.CharField')(max_length=250)),
-            ('categoria', self.gf('django.db.models.fields.CharField')(max_length=60)),
-            ('rutaImagen', self.gf('django.db.models.fields.CharField')(max_length=150)),
-            ('propietario', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['publicidadGeolocalizada.PuntoDeInteres'])),
-        ))
-        db.send_create_signal('publicidadGeolocalizada', ['Anuncio'])
-
+        pass
 
     def backwards(self, orm):
-        # Deleting model 'PuntoDeInteres'
-        db.delete_table('publicidadGeolocalizada_puntodeinteres')
-
-        # Deleting model 'Anuncio'
-        db.delete_table('publicidadGeolocalizada_anuncio')
-
+        pass
 
     models = {
-        'publicidadGeolocalizada.anuncio': {
-            'Meta': {'object_name': 'Anuncio'},
-            'categoria': ('django.db.models.fields.CharField', [], {'max_length': '60'}),
-            'descripcion': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'propietario': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['publicidadGeolocalizada.PuntoDeInteres']"}),
-            'rutaImagen': ('django.db.models.fields.CharField', [], {'max_length': '150'}),
-            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '70'})
-        },
-        'publicidadGeolocalizada.puntodeinteres': {
-            'Meta': {'object_name': 'PuntoDeInteres'},
-            'correoElectronico': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
-            'descripcion': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'nombre': ('django.db.models.fields.CharField', [], {'max_length': '70'}),
-            'paginaWeb': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
-            'posicion': ('django.contrib.gis.db.models.fields.PointField', [], {}),
-            'rutaImagen': ('django.db.models.fields.CharField', [], {'max_length': '150'}),
-            'telefono': ('django.db.models.fields.CharField', [], {'max_length': '10'})
-        }
+        
     }
 
     complete_apps = ['publicidadGeolocalizada']
--- publicidadGeolocalizada/models.py
+++ publicidadGeolocalizada/models.py
@@ -1,19 +1,31 @@
 from django.db import models
 from django.contrib.gis.db import models
+from django.contrib.auth.models import User
 # Create your models here.
+
+
 class PuntoDeInteres(models.Model):
-    nombre = models.CharField(max_length=70)
-    descripcion = models.CharField(max_length=250)
+    nombre = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=500)
     telefono = models.CharField(max_length=10)
-    paginaWeb = models.CharField(max_length=80)
-    correoElectronico = models.CharField(max_length=80)
-    rutaImagen = models.CharField(max_length=150)
+    direccion = models.CharField(max_length=310)
+    categoria = models.CharField(max_length=120)
+    paginaWeb = models.CharField(max_length=500)
+    correoElectronico = models.CharField(max_length=120)
+    rutaImagen = models.CharField(max_length=310)
     posicion = models.PointField()
     objects = models.GeoManager()
+    favoritos = models.ManyToManyField(User,editable=False, related_name = 'favoritos')
+    pripietario = models.ForeignKey(User, editable=False, related_name = 'propiedades')           
+     
+
+
 
 class Anuncio(models.Model):
-    titulo = models.CharField(max_length=70)
-    descripcion = models.CharField(max_length=250)
-    categoria = models.CharField(max_length=60)
-    rutaImagen = models.CharField(max_length=150)
-    propietario = models.ForeignKey(PuntoDeInteres)
+    anunciante = models.ForeignKey(PuntoDeInteres)
+    titulo = models.CharField(max_length=180)
+    descripcion = models.CharField(max_length=500)
+    categoria = models.CharField(max_length=120)
+    rutaImagen = models.CharField(max_length=500)
+    
+    
