46d6a3afeb2685d55d413984a4d742b8f02111c2
Russel Abraham Vela Miam <vela.russel@gmail.com>
2013-02-11 15:57:42 -0600

    Especificados los códigos de respuesta de la api del usuario para identificar los problemas ocurridos.

--- publicidadGeolocalizada/api_Usuario.py
+++ publicidadGeolocalizada/api_Usuario.py
@@ -3,17 +3,18 @@ import re
 
 def registrarUsuario(correo_e,password):
     correoValido=esCorreoValido(correo_e);
-    correoNuevo=True;#esCorreoNuevo(correo_e);
     contraseniaValida=esContraseniaValida(password);
-    if correoValido & correoNuevo & contraseniaValida:
-        nuevoUsuario=Usuario(correoElectronico=correo_e,contrasenia=password);
-        try:
-            nuevoUsuario.save();
-            return True;
-        except Exception,err:
-            return False;
-    else:
-        return False;
+    if not correoValido:
+        return 1;#el correo electrónico no cumple la forma especificada
+    if not contraseniaValida:
+        return 2;#la contrasenia no cumple los requisitos
+    
+    nuevoUsuario=Usuario(correoElectronico=correo_e,contrasenia=password);
+    try:
+        nuevoUsuario.save();
+        return 0;#Usuario creado y registrado
+    except Exception,err:
+        return 3;#el correo ya existe
 
 def esCorreoValido(correo_e):
     regex=re.compile('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$');
--- publicidadGeolocalizada/view_usuario.py
+++ publicidadGeolocalizada/view_usuario.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render_to_response;
 from django.http import HttpResponse;
 from utilidades import *;
 from django.template import RequestContext;
@@ -11,11 +10,14 @@ def peticionRegistrarUsuario(request):
         exito,parametros=extract_params(request.POST,CAMPOS_REGISTRAR_USUARIO);
         if(exito):
             registroExitoso=registrarUsuario(parametros["correo"],parametros["contrasenia"]);
-            if(registroExitoso):
-                return render_to_json("PDI/respuesta/error.json",{'codigo':100, 'mensaje':Usuario_Registrado});
-            else:
-                return render_to_json("PDI/respuesta/error.json",{'codigo':200, 'mensaje':err});
-        else:
-            return render_to_json("PDI/respuesta/error.json",{'codigo':200, 'mensaje':err});
+            if(registroExitoso==0):
+                return render_to_json("PDI/respuesta/error.json",{'codigo':100, 'mensaje':'Usuario Registrado.'});          
+            if(registroExitoso==1):
+                return render_to_json("PDI/respuesta/error.json",{'codigo':200, 'mensaje':'Introduzca un correo electrónico válido.'});
+            if(registroExitoso==2):            
+                return render_to_json("PDI/respuesta/error.json",{'codigo':200, 'mensaje':'La contraseña debe tener al menos 8 caracteres, una minúscula, una mayúscula y un dígito.'});
+            if(registroExitoso==3):        
+                return render_to_json("PDI/respuesta/error.json",{'codigo':200, 'mensaje':'El correo introducido ya ha sido registrado en el sistema.'});
     else:
-        return render_to_json("PDI/respuesta/error.json",{'codigo':200, 'mensaje':err});    
+        return render_to_json("PDI/respuesta/error.json",{'codigo':200, 'mensaje':'No se pueden procesar peticiones por GET.'});
+                    
